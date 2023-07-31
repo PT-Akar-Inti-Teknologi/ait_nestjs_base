527e7b66bc3e6b968c349b4869a372f75c297597
nfajardwiputra <nurrahman.fajar@akarinti.tech>
2023-08-01 01:38:45 +0700

    feat(test): add boostrap app test

---
+++ test/AppFactoryTest.ts
@@ -0,0 +1,16 @@
+import { ConfigHostModule } from '@nestjs/config/dist/config-host.module';
+
+import { AppFactory, Application } from 'src/AppFactory';
+
+describe('AppFactory', () => {
+  it('RUN', async () => {
+    const defaultDecorator = Application({});
+    await expect(defaultDecorator(AppFactory)).resolves.not.toThrow();
+
+    const decoratorWithOption = Application({
+      port: 2000,
+      imports: [ConfigHostModule],
+    });
+    await expect(decoratorWithOption(AppFactory)).resolves.not.toThrow();
+  });
+});
