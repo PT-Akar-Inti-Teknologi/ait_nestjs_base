d0ea63d03961ba6e9833f6d44fc27c7d42ae332d
Nie Ridwan Susanto <ridwan@akarinti.tech>
2023-11-01 12:19:33 +0700

    fix: AitMessageModule config

--- libs/message/interfaces/message-config.interface.ts
+++ libs/message/interfaces/message-config.interface.ts
@@ -7,7 +7,20 @@ import {
   I18nOptions,
 } from 'nestjs-i18n/dist/interfaces/i18n-options.interface';
 
-export class AitMessageConfig implements I18nOptions {
+export abstract class AitMessageConfig {
+  /** default language to use */
+  fallbackLanguage: string;
+  /** will use i18n version of MessageService. please fill [translations] if this is false. */
+  useNestI18n: boolean;
+}
+
+export type AitMessageConfigImpl = AitMessageConfigI18n | AitMessageConfigLocal;
+
+export class AitMessageConfigI18n
+  extends AitMessageConfig
+  implements I18nOptions
+{
+  useNestI18n: true;
   fallbackLanguage: string;
   fallbacks?: { [key: string]: string };
   resolvers?: I18nOptionResolver[];
@@ -21,10 +34,11 @@ export class AitMessageConfig implements I18nOptions {
   validatorOptions?: ValidatorOptions;
   throwOnMissingKey?: boolean;
   typesOutputPath?: string;
+}
 
-  /// original properties
-  /** will use i18n version of MessageService. please fill [translations] if this is false. */
-  useNestI18n: boolean;
+export class AitMessageConfigLocal extends AitMessageConfig {
+  useNestI18n: false;
+  fallbackLanguage: string;
   /** will be used when not using i18n translations, default to defaultLanguage translations */
   translations?: Record<string, any>;
 }
--- libs/message/message.module.ts
+++ libs/message/message.module.ts
@@ -1,18 +1,17 @@
 import { Module, Global, DynamicModule } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { I18nModule } from 'nestjs-i18n';
-import { AitMessageConfig } from './interfaces';
+import { AitMessageConfig, AitMessageConfigImpl } from './interfaces';
 import { MessageI18nService } from './impl/message-i18n.service';
 import { MessageLocalService } from './impl/message-local.service';
 
 @Global()
 @Module({})
 export class AitMessageModule {
-  static register(config: AitMessageConfig): DynamicModule {
-    const { useNestI18n, ...i18nConfig } = config;
+  static register(config: AitMessageConfigImpl): DynamicModule {
     const imports = [];
-    if (useNestI18n) {
-      imports.push(I18nModule.forRoot(i18nConfig));
+    if (config.useNestI18n) {
+      imports.push(I18nModule.forRoot(config));
     }
     return {
       module: AitMessageModule,
@@ -24,7 +23,9 @@ export class AitMessageModule {
         },
         {
           provide: MessageService,
-          useClass: useNestI18n ? MessageI18nService : MessageLocalService,
+          useClass: config.useNestI18n
+            ? MessageI18nService
+            : MessageLocalService,
         },
       ],
       exports: [MessageService],
