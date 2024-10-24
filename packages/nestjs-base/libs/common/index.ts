12ae2762a8b4ea664dc9798315d0fc43838c12d4
Nie Ridwan Susanto <ridwan@akarinti.tech>
2024-10-24 09:26:42 +0700

    fix(common): include transformers to barrel export

--- packages/nestjs-base/libs/common/index.ts
+++ packages/nestjs-base/libs/common/index.ts
@@ -3,4 +3,5 @@ export * from './common.service';
 export * from './dto/i18n-main-paging.dto';
 export * from './dto/main-paging.dto';
 export * from './interceptors';
+export * from './transformers';
 export * from './validators';
